8ada2f1d7a7d3974ddb749581993d85174b77595
KalyaniRupeshRamteke <kalyani.chandane28@gmail.com>
2025-05-23 00:16:31 +0530

    Add files via upload

---
+++ Assignment_6/Assignment6_2.py
@@ -0,0 +1,13 @@
+def main():
+
+    i= 1
+    sum = 0
+    while(i <= 100):
+        if(i % 2 == 0):
+            sum = sum + i
+        i=i+1
+
+    print("Sum of even number between 1 to 100 : ",sum)
+
+if __name__ =="__main__":
+    main()
---
+++ Assignment_6/Assignment6_4.py
@@ -0,0 +1,12 @@
+def main():
+
+    print("Enter the number")
+    num = int(input())
+    fact = 1
+    for x in range(num,1,-1):
+        fact = fact*x
+
+    print("Factorial of ", num , " is:",fact)
+
+if __name__ == "__main__":
+    main()
