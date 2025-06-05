0bfe80b2b0167d9852be6d3cfa0205749418d5af
KalyaniRupeshRamteke <kalyani.chandane28@gmail.com>
2025-06-06 00:52:58 +0530

    Update Assignment14_9.py

--- Assignment_14/Assignment14_9.py
+++ Assignment_14/Assignment14_9.py
@@ -4,25 +4,29 @@ class Product:
         self.Name = name
         self.Price = price
 
-def __eq__(a,b):
-    if(a.Price == b.Price):
-        print(a.Name ,"and",b.Name,"price are same",end=" ")
-    else:
-        print(a.Name ,"and",b.Name,"price are not same",end=" ")
-    print("\n
-    ")
+    
+    def __eq__(self,b):
+        if(self.Price == b.Price):
+            print(self.Name ,"and",b.Name,"price are same",end=" ")
+        else:
+            print(self.Name ,"and",b.Name,"price are not same",end=" ")
+        print("\n")
+
 
 def main():
 
     product1 = Product("LCD",10000)
     product2 = Product("Mouse",750)
 
-    __eq__(product1,product2)
+    product2.__eq__(product1)
+
+
+    #__eq__(product1,product2)
 
     product1 = Product("HP Laptop Elite book",5000)
     product2 = Product("HP Laptop",5000)
 
-    __eq__(product1,product2)
+    product1.__eq__(product2)
 
 
 if __name__ == "__main__":
